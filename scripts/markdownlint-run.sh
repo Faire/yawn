86fed1c79d00f7295138ec57d146c70b962334e5
Luan Nico <luanpotter27@gmail.com>
2025-10-06 15:27:23 -0400

    chore: Allow markdown-run.sh script to be run with `--fix` flag

--- scripts/markdownlint-run.sh
+++ scripts/markdownlint-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-markdownlint . -p .github/markdown-lint/.markdownlintignore -c .github/markdown-lint/.markdownlint.yaml
+markdownlint . -p .github/markdown-lint/.markdownlintignore -c .github/markdown-lint/.markdownlint.yaml "$@"
