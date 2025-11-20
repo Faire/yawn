53a08d57740086b0fa3dda866666bdc5549dceeb
Emily O'Leary <emily.o-leary@faire.com>
2025-11-20 08:57:22 -0800

    Make table lambdas plural

--- yawn-database-test/src/test/kotlin/com/faire/yawn/database/YawnForeignAndCompositeKeyTest.kt
+++ yawn-database-test/src/test/kotlin/com/faire/yawn/database/YawnForeignAndCompositeKeyTest.kt
@@ -16,13 +16,13 @@ internal class YawnForeignAndCompositeKeyTest : BaseYawnDatabaseTest() {
     @Test
     fun `generate using the Id when foreign key not specified`() {
         transactor.open { session ->
-            val bookId = session.project(BookTable) { book ->
-                addEq(book.name, "Harry Potter")
-                project(book.id)
+            val bookId = session.project(BookTable) { books ->
+                addEq(books.name, "Harry Potter")
+                project(books.id)
             }.uniqueResult()!!
 
-            val bookRanking = session.query(BookRankingTable) { bookRanking ->
-                addEq(bookRanking.bestSeller.foreignKey, bookId)
+            val bookRanking = session.query(BookRankingTable) { bookRankings ->
+                addEq(bookRankings.bestSeller.foreignKey, bookId)
             }.uniqueResult()!!
 
             assertThat(bookRanking.ratingYear).isEqualTo(2007)
@@ -34,21 +34,21 @@ internal class YawnForeignAndCompositeKeyTest : BaseYawnDatabaseTest() {
     @Test
     fun `simplified foreign key equals syntax`() {
         transactor.open { session ->
-            val hpBookId = session.project(BookTable) { book ->
-                addEq(book.name, "Harry Potter")
-                project(book.id)
+            val hpBookId = session.project(BookTable) { books ->
+                addEq(books.name, "Harry Potter")
+                project(books.id)
             }.uniqueResult()!!
 
-            val hpRanking = session.query(BookRankingTable) { bookRanking ->
-                addEq(bookRanking.bestSeller, hpBookId)
+            val hpRanking = session.query(BookRankingTable) { bookRankings ->
+                addEq(bookRankings.bestSeller, hpBookId)
             }.uniqueResult()!!
 
             assertThat(hpRanking.ratingYear).isEqualTo(2007)
             assertThat(hpRanking.ratingMonth).isEqualTo(1)
             assertThat(hpRanking.bestSeller.id).isEqualTo(hpBookId)
 
-            val lordOfTheRingsRanking = session.query(BookRankingTable) { bookRanking ->
-                addNotEq(bookRanking.bestSeller, hpBookId)
+            val lordOfTheRingsRanking = session.query(BookRankingTable) { bookRankings ->
+                addNotEq(bookRankings.bestSeller, hpBookId)
             }.uniqueResult()!!
 
             assertThat(lordOfTheRingsRanking.ratingYear).isEqualTo(1966)
@@ -60,25 +60,25 @@ internal class YawnForeignAndCompositeKeyTest : BaseYawnDatabaseTest() {
     @Test
     fun `simplified foreign key in clause syntax`() {
         transactor.open { session ->
-            val hpBookId = session.project(BookTable) { book ->
-                addEq(book.name, "Harry Potter")
-                project(book.id)
+            val hpBookId = session.project(BookTable) { books ->
+                addEq(books.name, "Harry Potter")
+                project(books.id)
             }.uniqueResult()!!
 
-            val lordOfTheRingsBookId = session.project(BookTable) { book ->
-                addEq(book.name, "Lord of the Rings")
-                project(book.id)
+            val lordOfTheRingsBookId = session.project(BookTable) { books ->
+                addEq(books.name, "Lord of the Rings")
+                project(books.id)
             }.uniqueResult()!!
 
-            val singleInResult = session.query(BookRankingTable) { bookRanking ->
-                addIn(bookRanking.bestSeller, setOf(lordOfTheRingsBookId))
+            val singleInResult = session.query(BookRankingTable) { bookRankings ->
+                addIn(bookRankings.bestSeller, setOf(lordOfTheRingsBookId))
             }.list()
 
             assertThat(singleInResult.map { Pair(it.ratingYear, it.bestSeller.name) })
                 .containsExactly(Pair(1966, "Lord of the Rings"))
 
-            val multipleInResult = session.query(BookRankingTable) { bookRanking ->
-                addIn(bookRanking.bestSeller, setOf(hpBookId, lordOfTheRingsBookId))
+            val multipleInResult = session.query(BookRankingTable) { bookRankings ->
+                addIn(bookRankings.bestSeller, setOf(hpBookId, lordOfTheRingsBookId))
             }.list()
 
             assertThat(multipleInResult.map { Pair(it.ratingYear, it.bestSeller.name) })
@@ -91,15 +91,15 @@ internal class YawnForeignAndCompositeKeyTest : BaseYawnDatabaseTest() {
 
     @Test
     fun `simplified foreign key in clause and subquery syntax`() {
-        val subQuery = Yawn.createProjectedDetachedCriteria(PersonTable) { person ->
-            addIn(person.name, "J.K. Rowling", "Luan Nico")
+        val subQuery = Yawn.createProjectedDetachedCriteria(PersonTable) { people ->
+            addIn(people.name, "J.K. Rowling", "Luan Nico")
 
-            project(person.favoriteBook.foreignKey)
+            project(people.favoriteBook.foreignKey)
         }
 
         transactor.open { session ->
-            val bookRankings = session.query(BookRankingTable) { bookRanking ->
-                addIn(bookRanking.bestSeller, subQuery)
+            val bookRankings = session.query(BookRankingTable) { bookRankings ->
+                addIn(bookRankings.bestSeller, subQuery)
             }.list()
 
             assertThat(bookRankings.map { Pair(it.ratingYear, it.bestSeller.name) })
@@ -113,8 +113,8 @@ internal class YawnForeignAndCompositeKeyTest : BaseYawnDatabaseTest() {
     @Test
     fun `query on custom named non-ID foreign key works and respects foreign key type`() {
         transactor.open { session ->
-            val result = session.query(BookReviewTable) { bookReview ->
-                addEq(bookReview.book.foreignKey, "PR6039 .O32 L67")
+            val result = session.query(BookReviewTable) { bookReviews ->
+                addEq(bookReviews.book.foreignKey, "PR6039 .O32 L67")
             }.uniqueResult()!!
             assertThat(result.reviewText).isEqualTo("Frodo was pretty cool.")
             assertThat(result.reviewer.name).isEqualTo("John Doe")
@@ -124,32 +124,32 @@ internal class YawnForeignAndCompositeKeyTest : BaseYawnDatabaseTest() {
     @Test
     fun `queries work with partial or whole composite IDs`() {
         transactor.open { session ->
-            val hpBookId = session.project(BookTable) { book ->
-                addEq(book.name, "Harry Potter")
-                project(book.id)
+            val hpBookId = session.project(BookTable) { books ->
+                addEq(books.name, "Harry Potter")
+                project(books.id)
             }.uniqueResult()!!
 
-            val luanId = session.project(PersonTable) { person ->
-                addEq(person.name, "Luan Nico")
-                project(person.id)
+            val luanId = session.project(PersonTable) { people ->
+                addEq(people.name, "Luan Nico")
+                project(people.id)
             }.uniqueResult()!!
 
-            val simplePartialQueryResult = session.query(BookCoverTable) { bookCover ->
-                addEq(bookCover.cid.bookId, hpBookId)
+            val simplePartialQueryResult = session.query(BookCoverTable) { bookCovers ->
+                addEq(bookCovers.cid.bookId, hpBookId)
             }.uniqueResult()!!
 
             assertThat(simplePartialQueryResult.inscription).isEqualTo("Harry Potter and the Sorcerer's Stone")
             assertThat(simplePartialQueryResult.owner.name).isEqualTo("Luan Nico")
 
-            val decomposedKeyQueryResult = session.query(BookCoverTable) { bookCover ->
-                addEq(bookCover.cid.bookId, hpBookId)
-                addEq(bookCover.cid.ownerId, luanId)
+            val decomposedKeyQueryResult = session.query(BookCoverTable) { bookCovers ->
+                addEq(bookCovers.cid.bookId, hpBookId)
+                addEq(bookCovers.cid.ownerId, luanId)
             }.uniqueResult()!!
 
             assertThat(simplePartialQueryResult).isEqualTo(decomposedKeyQueryResult)
 
-            val directCompositeIdQueryResult = session.query(BookCoverTable) { bookCover ->
-                addEq(bookCover.cid, BookCoverCompositeId(hpBookId, luanId))
+            val directCompositeIdQueryResult = session.query(BookCoverTable) { bookCovers ->
+                addEq(bookCovers.cid, BookCoverCompositeId(hpBookId, luanId))
             }.uniqueResult()!!
 
             assertThat(directCompositeIdQueryResult).isEqualTo(directCompositeIdQueryResult)
@@ -158,8 +158,8 @@ internal class YawnForeignAndCompositeKeyTest : BaseYawnDatabaseTest() {
                 .list()
                 .map { it.cid }
 
-            val inQueryResult = session.query(BookCoverTable) { bookCover ->
-                addIn(bookCover.cid, allBookCoverIds)
+            val inQueryResult = session.query(BookCoverTable) { bookCovers ->
+                addIn(bookCovers.cid, allBookCoverIds)
             }.list()
 
             assertThat(inQueryResult.map { it.inscription }).containsExactlyInAnyOrder(
@@ -172,23 +172,23 @@ internal class YawnForeignAndCompositeKeyTest : BaseYawnDatabaseTest() {
     @Test
     fun `foreign key queries work with composite foreign keys`() {
         transactor.open { session ->
-            val hpBookId = session.project(BookTable) { book ->
-                addEq(book.name, "Harry Potter")
-                project(book.id)
+            val hpBookId = session.project(BookTable) { books ->
+                addEq(books.name, "Harry Potter")
+                project(books.id)
             }.uniqueResult()!!
 
-            val luanId = session.project(PersonTable) { person ->
-                addEq(person.name, "Luan Nico")
-                project(person.id)
+            val luanId = session.project(PersonTable) { people ->
+                addEq(people.name, "Luan Nico")
+                project(people.id)
             }.uniqueResult()!!
-            val fullQueryResult = session.query(BookCoverRankingTable) { coverRanking ->
-                addEq(coverRanking.bookCover.foreignKey, BookCoverCompositeId(hpBookId, luanId))
+            val fullQueryResult = session.query(BookCoverRankingTable) { coverRankings ->
+                addEq(coverRankings.bookCover.foreignKey, BookCoverCompositeId(hpBookId, luanId))
             }.uniqueResult()!!
             assertThat(fullQueryResult.ranking).isEqualTo(2)
             assertThat(fullQueryResult.judgesComments).isEqualTo("Good construction and solid line work")
 
-            val partialQueryResult = session.query(BookCoverRankingTable) { coverRanking ->
-                addEq(coverRanking.bookCover.foreignKey.ownerId, luanId)
+            val partialQueryResult = session.query(BookCoverRankingTable) { coverRankings ->
+                addEq(coverRankings.bookCover.foreignKey.ownerId, luanId)
             }.uniqueResult()
             assertThat(partialQueryResult).isEqualTo(fullQueryResult)
         }
