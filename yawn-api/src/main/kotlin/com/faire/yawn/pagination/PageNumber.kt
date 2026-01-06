e331315d9a8774808559c23fbb372340749c3430
Luan Nico <luanpotter27@gmail.com>
2026-01-06 07:48:13 -0800

    feat: Add Long overloads to pagination model constructors (#93)

--- yawn-api/src/main/kotlin/com/faire/yawn/pagination/PageNumber.kt
+++ yawn-api/src/main/kotlin/com/faire/yawn/pagination/PageNumber.kt
@@ -16,10 +16,14 @@ value class PageNumber private constructor(val zeroIndexedPageNumber: Int) {
         get() = zeroIndexedPageNumber + 1
 
     operator fun div(pageSize: Int): Page = Page(pageNumber = this, pageSize = pageSize)
+    operator fun div(pageSize: Long): Page = this / pageSize.toInt()
 
     companion object {
         fun zeroIndexed(pageNumber: Int): PageNumber = PageNumber(zeroIndexedPageNumber = pageNumber)
+        fun zeroIndexed(pageNumber: Long): PageNumber = zeroIndexed(pageNumber.toInt())
+
         fun oneIndexed(pageNumber: Int): PageNumber = PageNumber(zeroIndexedPageNumber = pageNumber - 1)
+        fun oneIndexed(pageNumber: Long): PageNumber = oneIndexed(pageNumber.toInt())
 
         fun starting(): PageNumber = PageNumber(zeroIndexedPageNumber = 0)
     }
