3562fb45e813d0810a8c94899aadc627f58dd04f
Luan Nico <luanpotter27@gmail.com>
2026-09-03 09:50:28 -0400

    feat: Let a YawnProjector be used as YawnQueryProjection, for interop (#151)

--- yawn-api/src/main/kotlin/com/faire/yawn/project/YawnProjector.kt
+++ yawn-api/src/main/kotlin/com/faire/yawn/project/YawnProjector.kt
@@ -1,5 +1,8 @@
 package com.faire.yawn.project
 
+import com.faire.yawn.query.YawnCompilationContext
+import org.hibernate.criterion.Projection
+
 /**
  * A projection descriptor that can be resolved into a flat list of [ProjectionNode.Value] nodes for query compilation.
  *
@@ -7,11 +10,26 @@ package com.faire.yawn.project
  * [ProjectorResolver] resolution engine then walks this tree, flattening composites, eliminating constants and mapped
  * transforms, deduplicating identical leaves, and producing a [ResolvedProjection] that the query factory can compile.
  *
+ * Extends [YawnQueryProjection] so that a projector can be handed to anything that still speaks the older
+ * interface - `project(...)` above all - without every such signature needing an overload. That is what lets the
+ * two coexist while projections migrate onto [ProjectionNode].
+ *
+ * Note that [compile] and [project] resolve the tree on **every** call, and [project] is called once per result
+ * row. Callers that hold on to a projection should resolve it once up front instead.
+ *
  * @param SOURCE the type of the entity being queried.
  * @param TO the result type of this projection.
  */
-fun interface YawnProjector<SOURCE : Any, TO> {
+fun interface YawnProjector<SOURCE : Any, TO> : YawnQueryProjection<SOURCE, TO> {
     fun projection(): ProjectionNode<SOURCE, TO>
+
+    override fun compile(context: YawnCompilationContext): Projection = resolve().compile(context)
+
+    override fun project(value: Any?): TO = resolve().project(value)
+
+    private fun resolve(): ResolvedProjectionAdapter<SOURCE, TO> {
+        return ResolvedProjectionAdapter(ProjectorResolver<SOURCE>().resolve(this))
+    }
 }
 
 /**
